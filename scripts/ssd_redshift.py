0271cb314d731a5772cd94b01e7425557177a506
ElyonFraq <robert.schmidt95@web.de>
2026-04-19 21:52:18 +0200

    Create ssd_redshift.py

---
+++ scripts/ssd_redshift.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from ssd_core import a_eff
+
+
+def redshift_from_aeff(a):
+    """
+    Convert effective scale factor to redshift:
+    z = 1/a - 1
+    """
+    return (1.0 / a) - 1.0
+
+
+def z_of_t(t, params):
+    """
+    Compute redshift as a function of t
+    using the SSD effective scale factor.
+    """
+    a = a_eff(t, params)
+    return redshift_from_aeff(a)
+
+
+if __name__ == "__main__":
+    from ssd_core import SSDParameters
+
+    t = np.linspace(0.1, 2.0, 10)
+    params = SSDParameters()
+
+    z = z_of_t(t, params)
+    print("z(t) sample:", z)
